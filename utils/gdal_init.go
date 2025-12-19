989fc980a13d849be2159abcca5b604cab60fe02
shirei220 <110725093+shirei220@users.noreply.github.com>
2025-12-19 12:48:02 +1000

    got rid of unused vars for specific drivers as go apparently doesn't allow it

--- utils/gdal_init.go
+++ utils/gdal_init.go
@@ -32,31 +32,5 @@ func setDefaultEnv(envVar string, defaultVal string) {
 }
 
 func registerGDALDrivers() {
-	// This is a bit nasty, but this is one way to work out which
-	// drivers are present in the GDAL shared library. We then
-	// load the drivers of interest and then load all of the
-	// drivers. This places common drivers at the front of the
-	// driver list.
-	var haveNetCDF, haveHDF4, haveHDF5, haveJP2OpenJPEG bool
-	var haveGTiff, haveGSKYNetCDF bool
-
-	// Find out which drivers are present
 	C.GDALAllRegister()
-	for i := 0; i < int(C.GDALGetDriverCount()); i++ {
-		driver := C.GDALGetDriver(C.int(i))
-		switch C.GoString(C.GDALGetDriverShortName(driver)) {
-		case "GSKY_netCDF":
-			haveGSKYNetCDF = true
-		case "netCDF":
-			haveNetCDF = true
-		case "HDF4":
-			haveHDF4 = true
-		case "HDF5":
-			haveHDF5 = true
-		case "JP2OpenJPEG":
-			haveJP2OpenJPEG = true
-		case "GTiff":
-			haveGTiff = true
-		}
-	}
 }
